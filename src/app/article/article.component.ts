a2dc4b1322134b0ec0b88e1542acdafe696d29fe
Will <doggy.huang@gmail.com>
2016-08-27 15:29:31 +0800

    18 練習 Angular 2 元件之間的輸入輸出：@Output()

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -9,6 +9,13 @@ export class ArticleComponent implements OnInit {
 
   @Input()
   item: any;
+
+  @Output()
+  delete = new EventEmitter<any>();
+
+  // deleteArticle(item) {
+  //   this.delete.emit(item);
+  // }
 
   constructor() { }
 
